5e1dbc5c4a6655a4a060d6e90436cd612beb96dc
wangjiyong <wangjy_w@126.com>
2022-02-11 00:28:10 +0800

    update ui

---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,88 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+defineProps<{
+    icon: string;
+    name: string;
+    label: string[];
+    snapDate: string;
+    airdropDate: string;
+    description: string;
+    link: string;
+    amount: number;
+    loading?: boolean;
+}>();
+</script>
+
+<template>
+    <card class="card-bordered bg-base-100 border-2 hover:border-yellow-400">
+        <div class="card-body">
+            <div class="card-title flex items-center">
+                <div class="avatar">
+                    <div class="rounded-full w-10 h-10 ring ring-primary ring-offset-base-100">
+                        <img :src="icon" />
+                    </div>
+                </div>
+                <h4 class="ml-3">{{ name }}</h4>
+            </div>
+
+            <div class="mb-2 space-x-2 card-actions">
+                <badge v-for="item in label" :key="item" class="badge-ghost"> {{ item }}</badge>
+            </div>
+            <div class="grid grid-cols-2 gap-4">
+                <div>
+                    <p class="text-xs">快照时间</p>
+                    <p class="text-yellow-500">{{ snapDate }}</p>
+                </div>
+                <div>
+                    <p class="text-xs">空投时间</p>
+                    <p class="text-yellow-500">{{ airdropDate }}</p>
+                </div>
+            </div>
+
+            <p class="mt-2">{{ description }}</p>
+            <div class="justify-end space-x-1 card-actions">
+                <button v-if="loading" class="btn btn-outline btn-primary loading">loading</button>
+
+                <div v-else>
+                    <button v-if="amount" class="btn btn-primary">
+                        <h3 class="text">{{ amount }}</h3
+                        ><span class="ml-1">CLAIM</span>
+
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            class="inline-block w-3 h-3 ml-1 stroke-current"
+                        >
+                            <path
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                stroke-width="2"
+                                d="M9 5l7 7-7 7"
+                            ></path>
+                        </svg>
+                    </button>
+                    <button v-else class="btn btn-outline">
+                        <span class="ml-1">not eligible > DETAIL</span>
+
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            class="inline-block w-3 h-3 ml-1 stroke-current"
+                        >
+                            <path
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                stroke-width="2"
+                                d="M9 5l7 7-7 7"
+                            ></path>
+                        </svg>
+                    </button>
+                </div>
+            </div>
+        </div>
+    </card>
+</template>
+
+<style scoped></style>
--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -1,59 +1,167 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import ProjectCardVue from '@/components/ProjectCard.vue';
+import ProjectFooter from '@/components/ProjectFooter.vue';
+components: {
+    ProjectCardVue;
+    ProjectFooter;
+}
+const projectList = ref([
+    {
+        icon: 'http://daisyui.com/tailwind-css-component-profile-1@56w.png',
+        name: 'Top 10 UI Components',
+        label: ['Colors', 'UI Design', 'Creativity'],
+        snapDate: '2021-11-11',
+        airdropDate: '2022-01-11',
+        description:
+            'Rerum reiciendis beatae tenetur excepturi aut pariatur est eos. Sit sit necessitatibus veritatis sed molestiae voluptates incidunt iure sapiente.',
+        link: '',
+        amount: 6666,
+        loading: true
+    },
+    {
+        icon: 'http://daisyui.com/tailwind-css-component-profile-1@56w.png',
+        name: 'Top 10 UI Components',
+        label: ['Colors', 'UI Design', 'Creativity'],
+        snapDate: '2021-11-11',
+        airdropDate: '2022-01-11',
+        description:
+            'Rerum reiciendis beatae tenetur excepturi aut pariatur est eos. Sit sit necessitatibus veritatis sed molestiae voluptates incidunt iure sapiente.',
+        link: '',
+        amount: 0,
+        loading: true
+    },
+    {
+        icon: 'http://daisyui.com/tailwind-css-component-profile-1@56w.png',
+        name: 'Top 10 UI Components',
+        label: ['Colors', 'UI Design', 'Creativity'],
+        snapDate: '2021-11-11',
+        airdropDate: '2022-01-11',
+        description:
+            'Rerum reiciendis beatae tenetur excepturi aut pariatur est eos. Sit sit necessitatibus veritatis sed molestiae voluptates incidunt iure sapiente.',
+        link: '',
+        amount: 6666,
+        loading: false
+    },
+    {
+        icon: 'http://daisyui.com/tailwind-css-component-profile-1@56w.png',
+        name: 'Top 10 UI Components',
+        label: ['Colors', 'UI Design', 'Creativity'],
+        snapDate: '2021-11-11',
+        airdropDate: '2022-01-11',
+        description:
+            'Rerum reiciendis beatae tenetur excepturi aut pariatur est eos. Sit sit necessitatibus veritatis sed molestiae voluptates incidunt iure sapiente.',
+        link: '',
+        amount: 0,
+        loading: false
+    },
+    {
+        icon: 'http://daisyui.com/tailwind-css-component-profile-1@56w.png',
+        name: 'Top 10 UI Components',
+        label: ['Colors', 'UI Design', 'Creativity'],
+        snapDate: '2021-11-11',
+        airdropDate: '2022-01-11',
+        description:
+            'Rerum reiciendis beatae tenetur excepturi aut pariatur est eos. Sit sit necessitatibus veritatis sed molestiae voluptates incidunt iure sapiente.',
+        link: '',
+        amount: 6666
+    },
+    {
+        icon: 'http://daisyui.com/tailwind-css-component-profile-1@56w.png',
+        name: 'Top 10 UI Components',
+        label: ['Colors', 'UI Design', 'Creativity'],
+        snapDate: '2021-11-11',
+        airdropDate: '2022-01-11',
+        description:
+            'Rerum reiciendis beatae tenetur excepturi aut pariatur est eos. Sit sit necessitatibus veritatis sed molestiae voluptates incidunt iure sapiente.',
+        link: '',
+        amount: 0
+    }
+]);
+</script>
 <template>
-    <div class="flex justify-center">
-        <div class="grid grid-cols-1 p-4 gap-4 max-w-6xl lg:grid-cols-3 lg:p-10 rounded-box">
-            <card
-                v-for="(item, index) in 7"
-                :key="index"
-                class="card-bordered bg-base-100 border-2"
-            >
-                <div class="card-body">
-                    <div class="card-title flex items-center">
-                        <div class="avatar">
-                            <div
-                                class="rounded-full w-10 h-10 ring ring-primary ring-offset-base-100"
-                            >
-                                <img
-                                    src="http://daisyui.com/tailwind-css-component-profile-1@56w.png"
-                                />
-                            </div>
-                        </div>
-                        <h4 class="ml-3">Top 10 UI Components</h4>
-                    </div>
+    <div>
+        <div
+            id="nav"
+            class="flex justify-end items-center border-base-200 bg-base-100 text-base-content sticky inset-x-0 top-0 z-50 w-full transition duration-200 ease-in-out border-b"
+        >
+            <div class="flex-none ml-6"><img width="200" src="../../assets/logo.png" /></div>
 
-                    <div class="mb-2 space-x-2 card-actions">
-                        <badge class="badge-ghost">Colors</badge>
-                        <badge class="badge-ghost">UI Design</badge>
-                        <badge class="badge-ghost">Creativity</badge>
+            <div class="flex-1">
+                <!-- <div class="flex space-x-2 mx-8 hidden lg:mx-40 lg:flex">
+                    <input
+                        type="text"
+                        placeholder="Search"
+                        class="w-full input input-primary input-bordered"
+                    />
+                    <button class="btn btn-primary">go</button>
+                </div> -->
+            </div>
+            <div class="navbar max-w-none">
+                <div class="dropdown dropdown-end" title="Change Theme">
+                    <div tabindex="0" class="m-1 normal-case btn-ghost btn">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            class="inline-block w-6 h-6 stroke-current md:mr-2"
+                        >
+                            <path
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                stroke-width="2"
+                                d="M7 21a4 4 0 01-4-4V5a2 2 0 012-2h4a2 2 0 012 2v12a4 4 0 01-4 4zm0 0h12a2 2 0 002-2v-4a2 2 0 00-2-2h-2.343M11 7.343l1.657-1.657a2 2 0 012.828 0l2.829 2.829a2 2 0 010 2.828l-8.486 8.485M7 17h.01"
+                            />
+                        </svg>
+                        <span class="hidden md:inline"> Manage Profile </span>
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            class="inline-block w-4 h-4 ml-1 fill-current"
+                            viewBox="0 0 1792 1792"
+                        >
+                            <path
+                                d="M1395 736q0 13-10 23l-466 466q-10 10-23 10t-23-10l-466-466q-10-10-10-23t10-23l50-50q10-10 23-10t23 10l393 393 393-393q10-10 23-10t23 10l50 50q10 10 10 23z"
+                            />
+                        </svg>
                     </div>
-                    <p
-                        >Rerum reiciendis beatae tenetur excepturi aut pariatur est eos. Sit sit
-                        necessitatibus veritatis sed molestiae voluptates incidunt iure sapiente.</p
+                    <div
+                        class="mt-16 overflow-y-auto shadow-2xl top-px dropdown-content h-96 w-52 rounded-b-box bg-base-200 text-base-content"
+                        v-bind:class="{
+                            'rounded-t-box': isHomepage && !toggleNavClass()
+                        }"
                     >
-                    <div class="justify-end space-x-1 card-actions">
-                        <button class="btn btn-square loading"></button>
-
-                        <button class="btn">
-                            <h3 class="text-primary">4,600</h3><span class="ml-1">CLAIM</span>
-
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                fill="none"
-                                viewBox="0 0 24 24"
-                                class="inline-block w-3 h-3 ml-1 stroke-current"
-                            >
-                                <path
-                                    stroke-linecap="round"
-                                    stroke-linejoin="round"
-                                    stroke-width="2"
-                                    d="M9 5l7 7-7 7"
-                                ></path>
-                            </svg>
-                        </button>
+                        <ul class="p-4 menu compact">
+                            <li v-for="(theme, index) in themes">
+                                <a
+                                    tabindex="0"
+                                    :data-set-theme="theme.id"
+                                    data-act-class="active"
+                                    >{{ theme.name }}</a
+                                >
+                            </li>
+                        </ul>
                     </div>
                 </div>
-            </card>
+            </div>
         </div>
+
+        <div class="flex justify-center">
+            <div class="grid grid-cols-1 p-4 gap-4 max-w-6xl lg:grid-cols-3 lg:p-10 rounded-box">
+                <project-card-vue
+                    v-for="(item, index) in projectList"
+                    :key="index"
+                    :airdrop-date="item.airdropDate"
+                    :snap-date="item.snapDate"
+                    :amount="item.amount"
+                    :icon="item.icon"
+                    :description="item.description"
+                    :label="item.label"
+                    :name="item.name"
+                    :link="item.link"
+                    :loading="item.loading"
+                ></project-card-vue>
+            </div>
+        </div>
+        <project-footer></project-footer>
     </div>
 </template>
-
-<script></script>
