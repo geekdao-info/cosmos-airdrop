260dfad6f99be533a5f82f05049d92275cea3c10
fomoFi.icu <95140885+fomofi@users.noreply.github.com>
2022-03-02 19:47:15 +0800

    修改样式

--- src/components/projectInfo/LikeInfo.vue
+++ src/components/projectInfo/LikeInfo.vue
@@ -1,6 +1,281 @@
 <template>
     <div>
-        If you are ATOM/OSMO holders or Civic Liker before 2021.11.30, you can claim LIKE and
-        participate in LikeCoin governance.
+        <section
+            ><div
+                ><div class="eg as hc hd he hf"></div
+                ><div class="hg hh hi hj hk"
+                    ><div class=""
+                        ><h2
+                            id="39cb"
+                            class="pw-subtitle-paragraph ik hm hn cg b il im in io ip iq ir is it iu iv iw ix iy iz ja jb bw"
+                            data-selectable-paragraph=""
+                            >Unlike any recent airdrop, we intended to design it to be a “fairdrop”
+                            for holders that believe in decentralized publishing and our potential
+                            in a long run.</h2
+                        ></div
+                    ><figure class="jd je jf jg ga jh fg fh paragraph-image"
+                        ><div role="button" tabindex="0" class="ji jj cj jk dq jl"
+                            ><div class="fg fh jc"
+                                ><img
+                                    alt=""
+                                    class="dq jm jn"
+                                    src="https://miro.medium.com/max/1400/1*KXxhvWaWguLNZAg8Ng8v2w.png"
+                                    width="700"
+                                    height="368"
+                                    role="presentation" /></div></div></figure
+                    ><p
+                        id="69cc"
+                        class="pw-post-body-paragraph jo jp hn jq b jr js io jt ju jv ir jw jx jy jz ka kb kc kd ke kf kg kh ki kj hg et"
+                        data-selectable-paragraph=""
+                        >LikeCoin chain first landed and launched on Cosmos in 2019. We have been
+                        tackling problems with writers in places that lack press freedom. As of
+                        today, there are <strong class="jq ho">2,211,986</strong>
+                        <strong class="jq ho">registered content</strong> by
+                        <strong class="jq ho">24,000</strong>
+                        <strong class="jq ho">active addresses</strong> on
+                        <strong class="jq ho">8,576 media outlets</strong>, in the LikeCoin network.
+                        The idea of decentralized publishing is proven to be an important aspect for
+                        users and the future of Web 3 infrastructure. Read the original airdrop
+                        proposal
+                        <a
+                            class="au fq"
+                            rel="noopener ugc nofollow"
+                            target="_blank"
+                            href="https://blog.like.co/proposal-to-fairdrop-likecoin-to-the-cosmos-community-and-civic-likers-d64d841287d8"
+                            >here</a
+                        >.</p
+                    ></div
+                ><div class="o ct kk kl km kn" role="separator"
+                    ><span class="ko ed ck kp kq kr"></span><span class="ko ed ck kp kq kr"></span
+                    ><span class="ko ed ck kp kq"></span></div
+                ><div class="hg hh hi hj hk"
+                    ><h2
+                        id="7ff7"
+                        class="ks kt hn cg ku kv kw kx ky kz la lb lc jx ld le lf kb lg lh li kf lj lk ll lm et"
+                        data-selectable-paragraph=""
+                        >Here are some quick tips on how you can claim the airdrop.</h2
+                    ><p
+                        id="3f08"
+                        class="pw-post-body-paragraph jo jp hn jq b jr ln io jt ju lo ir jw jx lp jz ka kb lq kd ke kf lr kh ki kj hg et"
+                        data-selectable-paragraph=""
+                        >Visit the official<strong class="jq ho"> </strong
+                        ><a
+                            class="au fq"
+                            href="https://app.like.co/airdrop/"
+                            rel="noopener ugc nofollow"
+                            target="_blank"
+                            ><strong class="jq ho">LikeCoin airdrop website</strong></a
+                        ><strong class="jq ho">. </strong>Complete 4 missions to claim all the
+                        airdrop amounts. These missions are designed to help you understand the
+                        LikeCoin ecosystem better.</p
+                    ><ul class=""
+                        ><li
+                            id="0a6d"
+                            class="ls lt hn jq b jr js ju jv jx lu kb lv kf lw kj lx ly lz ma et"
+                            data-selectable-paragraph=""
+                            ><strong class="jq ho">Manage LikeCoin. </strong>Link your Keplr wallet
+                            to receive LIKE at the
+                            <a
+                                class="au fq"
+                                href="https://app.like.co/airdrop/"
+                                rel="noopener ugc nofollow"
+                                target="_blank"
+                                >airdrop page</a
+                            >.</li
+                        ><li
+                            id="2329"
+                            class="ls lt hn jq b jr mb ju mc jx md kb me kf mf kj lx ly lz ma et"
+                            data-selectable-paragraph=""
+                            ><strong class="jq ho">Decentralize Tweeting.</strong> Visit
+                            <a
+                                class="au fq"
+                                href="https://depub.space/"
+                                rel="noopener ugc nofollow"
+                                target="_blank"
+                                >depub.SPACE</a
+                            >, and publish a tweet.</li
+                        ><li
+                            id="46fa"
+                            class="ls lt hn jq b jr mb ju mc jx md kb me kf mf kj lx ly lz ma et"
+                            data-selectable-paragraph=""
+                            ><strong class="jq ho">Stake LikeCoin. </strong>Delegate LIKE via
+                            <a
+                                class="au fq"
+                                href="https://dao.like.co/validators"
+                                rel="noopener ugc nofollow"
+                                target="_blank"
+                                >dao.like.co</a
+                            >.</li
+                        ><li
+                            id="3afc"
+                            class="ls lt hn jq b jr mb ju mc jx md kb me kf mf kj lx ly lz ma et"
+                            data-selectable-paragraph=""
+                            ><strong class="jq ho">Vote on LikeCoin DAO.</strong> Vote on any
+                            <a
+                                class="au fq"
+                                href="https://dao.like.co/proposals"
+                                rel="noopener ugc nofollow"
+                                target="_blank"
+                                >proposals</a
+                            >.</li
+                        ></ul
+                    ><p
+                        id="9709"
+                        class="pw-post-body-paragraph jo jp hn jq b jr js io jt ju jv ir jw jx jy jz ka kb kc kd ke kf kg kh ki kj hg et"
+                        data-selectable-paragraph=""
+                        >The airdrop claim will last for 180 days till
+                        <strong class="jq ho">August 22, 2022</strong>. Since the 91st day,
+                        unclaimed fairdrop will decay linearly until it reaches 0 on the 181st day.
+                        Unclaimed LIKE will be returned to the Community Pool.</p
+                    ><p
+                        id="f9a5"
+                        class="pw-post-body-paragraph jo jp hn jq b jr js io jt ju jv ir jw jx jy jz ka kb kc kd ke kf kg kh ki kj hg et"
+                        data-selectable-paragraph=""
+                        >If you want to check the transactions during the claiming process, you can
+                        always go to block explorers like
+                        <a
+                            class="au fq"
+                            href="https://likecoin.bigdipper.live/"
+                            rel="noopener ugc nofollow"
+                            target="_blank"
+                            >Big Dipper</a
+                        >
+                        or
+                        <a
+                            class="au fq"
+                            href="https://dao.like.co"
+                            rel="noopener ugc nofollow"
+                            target="_blank"
+                            >dao.like.co</a
+                        >
+                        to view the records of the airdrops you have received.</p
+                    ><p
+                        id="2907"
+                        class="pw-post-body-paragraph jo jp hn jq b jr js io jt ju jv ir jw jx jy jz ka kb kc kd ke kf kg kh ki kj hg et"
+                        data-selectable-paragraph=""
+                        >A full airdrop guide is also available
+                        <a
+                            class="au fq"
+                            href="https://docs.like.co/airdrop"
+                            rel="noopener ugc nofollow"
+                            target="_blank"
+                            >here</a
+                        >.</p
+                    ></div
+                ><div class="o ct kk kl km kn" role="separator"
+                    ><span class="ko ed ck kp kq kr"></span><span class="ko ed ck kp kq kr"></span
+                    ><span class="ko ed ck kp kq"></span></div
+                ><div class="hg hh hi hj hk"
+                    ><h2
+                        id="72e9"
+                        class="ks kt hn cg ku kv kw kx ky kz la lb lc jx ld le lf kb lg lh li kf lj lk ll lm et"
+                        data-selectable-paragraph=""
+                        ><strong class="ba">Stake your LIKE to the Creator fund</strong></h2
+                    ><figure class="jd je jf jg ga jh fg fh paragraph-image"
+                        ><div role="button" tabindex="0" class="ji jj cj jk dq jl"
+                            ><div class="fg fh jc"
+                                ><img
+                                    alt=""
+                                    class="dq jm jn"
+                                    src="https://miro.medium.com/max/1400/1*cL7uIAwUiwETryOqDc87Pg.png"
+                                    width="700"
+                                    height="368"
+                                    role="presentation" /></div></div></figure
+                    ><p
+                        id="f2f8"
+                        class="pw-post-body-paragraph jo jp hn jq b jr js io jt ju jv ir jw jx jy jz ka kb kc kd ke kf kg kh ki kj hg et"
+                        data-selectable-paragraph=""
+                        ><strong class="jq ho"
+                            >Consider staking your LIKE to the civic liker node.</strong
+                        >
+                        Stakers will contribute to the distribution of the LikeCoin Creators Fund.
+                        And shall receive
+                        <a
+                            class="au fq"
+                            href="https://poap.xyz/faqs"
+                            rel="noopener ugc nofollow"
+                            target="_blank"
+                            >an NFT poap</a
+                        >
+                        to represent your contribution.
+                        <a
+                            class="au fq"
+                            href="https://liker.land/civic"
+                            rel="noopener ugc nofollow"
+                            target="_blank"
+                            >Learn more</a
+                        >.</p
+                    ></div
+                ><div class="o ct kk kl km kn" role="separator"
+                    ><span class="ko ed ck kp kq kr"></span><span class="ko ed ck kp kq kr"></span
+                    ><span class="ko ed ck kp kq"></span></div
+                ><div class="hg hh hi hj hk"
+                    ><p
+                        id="e3cd"
+                        class="pw-post-body-paragraph jo jp hn jq b jr js io jt ju jv ir jw jx jy jz ka kb kc kd ke kf kg kh ki kj hg et"
+                        data-selectable-paragraph=""
+                        >For questions about the airdrop, you can reach us on
+                        <a
+                            class="au fq"
+                            href="https://discord.gg/wx3GzYhjWf"
+                            rel="noopener ugc nofollow"
+                            target="_blank"
+                            >Discord</a
+                        >. Otherwise, welcome to DePub, let’s
+                        <a
+                            class="au fq"
+                            href="https://liker.land/civic/"
+                            rel="noopener ugc nofollow"
+                            target="_blank"
+                            >like</a
+                        >
+                        and
+                        <a
+                            class="au fq"
+                            href="https://depub.space/"
+                            rel="noopener ugc nofollow"
+                            target="_blank"
+                            >tweet</a
+                        >
+                        about us.</p
+                    ><h2
+                        id="e640"
+                        class="ks kt hn cg ku kv kw kx ky kz la lb lc jx ld le lf kb lg lh li kf lj lk ll lm et"
+                        data-selectable-paragraph=""
+                        ><a
+                            class="au fq"
+                            href="https://twitter.com/likecoin"
+                            rel="noopener ugc nofollow"
+                            target="_blank"
+                            >Twitter</a
+                        >
+                        |
+                        <a
+                            class="au fq"
+                            href="https://like.co/"
+                            rel="noopener ugc nofollow"
+                            target="_blank"
+                            >Website</a
+                        >
+                        |
+                        <a
+                            class="au fq"
+                            href="https://likecoin.bigdipper.live/"
+                            rel="noopener ugc nofollow"
+                            target="_blank"
+                            >Block Explorer</a
+                        >
+                        |
+                        <a
+                            class="au fq"
+                            href="https://discord.gg/W4DQ6peZZZ"
+                            rel="noopener ugc nofollow"
+                            target="_blank"
+                            >Discord</a
+                        ></h2
+                    ></div
+                ></div
+            ></section
+        >
     </div>
 </template>
